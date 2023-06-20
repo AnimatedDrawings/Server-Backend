49e73b90214fc0245730c7bf42b35e54b66fdf42
chminipark <chminipark@gmail.com>
2023-06-20 19:44:56 +0900

    [Test] : fastapi uvicorn host 수정

--- TestFastAPI/TestFastAPIVolume/main.py
+++ TestFastAPI/TestFastAPIVolume/main.py
@@ -26,4 +26,4 @@ def ping():
 #         return response.text
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', port=5000, reload=True)
+    uvicorn.run('main:app', host = '0.0.0.0', port=5000, reload=True)
