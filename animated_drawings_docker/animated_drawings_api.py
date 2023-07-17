0de25a9ea80bf45050998065700b530fc3354cc3
minii <chminipark@gmail.com>
2023-07-17 18:08:43 +0900

    [Feat] : AD image_to_annotations api 추가

--- animated_drawings_docker/animated_drawings_api.py
+++ animated_drawings_docker/animated_drawings_api.py
@@ -1,9 +1,10 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, request
 from image_to_annotations import image_to_annotations
 from annotations_to_animation import annotations_to_animation
 from pathlib import Path
 import logging
 from pkg_resources import resource_filename
+
 
 app = Flask(__name__)
 
@@ -11,23 +12,38 @@ app = Flask(__name__)
 def ping():
     return 'animated_drawings test ping success!!'
 
-@app.route('/get_my_animated_drawings')
-def get_my_animated_drawings():
-    log_dir = Path('./logs')
-    log_dir.mkdir(exist_ok=True, parents=True)
-    logging.basicConfig(filename=f'{log_dir}/log.txt', level=logging.DEBUG)
+ANNOTATIONS = '/mycode/files/annotations/'
 
-    img_fn = resource_filename(__name__, 'drawings/garlic.png')  
-    char_anno_dir = 'garlic_out'
-    motion_cfg_fn = resource_filename(__name__, 'config/motion/dab.yaml')    
-    retarget_cfg_fn = resource_filename(__name__, 'config/retarget/fair1_ppf.yaml')
+'''
+1. [Fast] masked file 로컬 저장
+2. [Fast] masked file_location postgres ad_db에 저장
+3. [AD]
+    1. image_to_annotations/
+    2. masked_file_url로 annotation file(char_cfg.yaml) 생성
+    3. annotation file url return
+    [Fast]
+    1. annotation file url postgres ad_db에 저장
+    2. annotation file url -> char_cfg.yaml -> Json 변환후 리턴
+'''
+@app.route('/image_to_annotations')
+def api_image_to_annotations():
+    parameter_dict = request.args.to_dict()
+    if len(parameter_dict) == 0:
+        return 'No parameter'
 
-    # create the annotations
-    image_to_annotations(img_fn, char_anno_dir)
-    # create the animation
-    annotations_to_animation(char_anno_dir, motion_cfg_fn, retarget_cfg_fn)
+    path = parameter_dict['path']
+    file_name = parameter_dict['file_name']
+    extension = parameter_dict['extension']
+    img_fn = path + file_name + extension
+    out_dir = ANNOTATIONS + file_name
+    image_to_annotations(img_fn = img_fn, out_dir = out_dir)
 
-    return 'animation success!'
+    annotation_info = {
+        'path' : ANNOTATIONS + file_name + '/',
+        'joint_filename' : 'char_cfg.yaml'
+    }
+
+    return annotation_info
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port='8001')
