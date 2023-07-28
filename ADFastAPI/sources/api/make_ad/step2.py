c75362ae1cefc206726c644e2df6d9a4a6c8d3d3
minii <chminipark@gmail.com>
2023-07-28 17:46:06 +0900

    [Refactor] : step2 return type 수정
    Git : #15

--- ADFastAPI/sources/api/make_ad/step2.py
+++ ADFastAPI/sources/api/make_ad/step2.py
@@ -33,9 +33,8 @@ async def find_the_character(ad_id: str, bounding_box: BoundingBox):
     async with httpx.AsyncClient() as client:
         response = await client.get(url = ad_url, params = params)
         try:
-            return "success.."
-            # bounding_box_dict = response.json()
-            # result = { 'ad_id' : ad_id, 'bounding_box' : bounding_box_dict }
-            # return result
+            bounding_box_dict = response.json()
+            result = { 'ad_id' : ad_id }
+            return result
         except:
             return response.text
